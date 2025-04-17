9940d4c734638e636db3e19f4fa324114d6becbc
Bektur Ibraimov <bekturibraimov21@gmail.com>
2025-04-17 19:53:31 +0600

    Added new class CardAcceptor.java and rewrote CoinAcceptor.java

--- src/paymentMethods/CoinAcceptor.java
+++ src/paymentMethods/CoinAcceptor.java
@@ -1,17 +1,28 @@
 package paymentMethods;
 
-public class CoinAcceptor {
-    private int amount;
+public class CoinAcceptor implements PaymentAcceptor{
+    private int balance;
 
-    public CoinAcceptor(int amount) {
-        this.amount = amount;
+    public CoinAcceptor(int initialBalance) {
+        this.balance = initialBalance;
     }
 
-    public int getAmount() {
-        return amount;
+    @Override
+    public boolean pay(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    @Override
+    public void addFunds(int amount) {
+        balance += amount;
+    }
+
+    @Override
+    public int getBalance() {
+        return balance;
     }
 }
